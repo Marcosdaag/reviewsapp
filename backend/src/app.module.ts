7d492497d08fdeeff63a140b8cf7176026cbab1c
Marcosdaag <marcosoffs99@gmail.com>
2025-12-03 12:23:08 -0300

    version 5 de prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './resources/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -3,7 +3,25 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
+    constructor() {
+        const url = process.env.DATABASE_URL;
+
+        if (!url) {
+            console.error('No se pudo cargar la variable de entorno URL');
+        } else {
+            console.log('Se cargo correctamente la URL');
+        }
+
+        super({
+            datasources: {
+                db: {
+                    url: url || '',
+                },
+            },
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
 }
